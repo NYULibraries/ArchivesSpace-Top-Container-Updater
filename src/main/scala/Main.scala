5870854672063ada019fab6cdb24139001698dc5
Donald Mennerich <don.mennerich@nyu.edu>
2018-08-29 15:29:41 -0400

    adding command line interface

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,60 +1,36 @@
-package edu.nyu.libraries.dlts
+package edu.nyu.libraries.dlts.aspace
 
-import com.typesafe.config._
+import CLI.CLISupport
+import Http.HttpSupport
+
 import java.io._
 import java.net.URI
-import org.apache.http.HttpEntity
-import org.apache.http.HttpResponse
-import org.apache.http.client.{ ClientProtocolException, ResponseHandler }
-import org.apache.http.client.config.RequestConfig
-import org.apache.http.client.methods.{ HttpPost, HttpGet }
-import org.apache.http.impl.client.{ HttpClientBuilder, CloseableHttpClient } 
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
-import org.apache.http.entity.StringEntity
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import org.json4s.DefaultFormats
+import org.rogach.scallop.exceptions._
 import scala.io.Source
-import org.rogach.scallop._
 
-object Main extends App {
+object Main extends App with CLISupport with HttpSupport{
 
-  implicit val formats = DefaultFormats
-
-  //initialize configurations
-  val conf = ConfigFactory.load()
-  val cli = new CLIConf(args)
-   
-  //initialize cli arguments
+  //initialize command line arguments
+  val cli = getCLI(args)  
   val csv = cli.source()
   val log = cli.log()
   val drop = cli.drop()
   val take = cli.take()
+ 
 
-  //initialize configuration file
-  val username = conf.getString("tcUpdater.username")
-  val password = conf.getString("tcUpdater.password")
-  val aspace = conf.getString("tcUpdater.aspaceApi")
-  
   //initialize logs
   val logger = new FileWriter(new File(log))
   val errorLogger = new FileWriter(new File("errors.txt"))
-  
-  //initialize httpclient
-  val timeout = 2;
-  val config = RequestConfig.custom()
-    .setConnectTimeout(timeout * 1000)
-    .setConnectionRequestTimeout(timeout * 1000)
-    .setSocketTimeout(timeout * 1000).build();
-  val client = HttpClientBuilder.create().setDefaultRequestConfig(config).build()
-  val header = "X-ArchivesSpace-Session"
-  
-  //get authenticate
-  val token = getToken
 
-  process
+  //get authenticate
   
+
+  //process the csv file
+  process
+
   def process() { 
     
 
@@ -71,7 +47,7 @@ object Main extends App {
 
       try{
 
-        val json = get(token, aspace + ao)
+        val json = get(ao).get
         val tc = json.\("instances")(0).\("sub_container").\("top_container").\("ref")
         val title = json.\("title").extract[String]
         println(i + s": $ao: $title")    
@@ -92,7 +68,7 @@ object Main extends App {
                 (updated.\("instances")(0).\("sub_container").\("top_container").\("ref") == newTC) match {
                   case true => {
                     val updatedJson = (compact(render(updated)))
-                    post(token, aspace + ao, updatedJson)           
+                    postJson(ao, updatedJson)           
                     logger.write(s"$ao $title updated $oldTC set to $newTC \n")
                     logger.flush
                   }
@@ -131,50 +107,4 @@ object Main extends App {
     client.close
   }
 
-  //methods
-  def getToken(): String = {
-    val authenticate = new HttpPost(aspace + "/users/" + username + "/login?password=" + password)
-    val response = client.execute(authenticate)
-    val entity = response.getEntity
-    val content = entity.getContent
-    val data = scala.io.Source.fromInputStream(content).mkString
-    val jarray = parse(data)
-    val askey = (jarray \ "session").extract[String]
-    EntityUtils.consume(entity)
-    response.close
-    askey
-  }
-
-  def get(token: String, url: String): JValue = {
-    val get = new HttpGet(url)
-    get.addHeader(header, token)
-    val response = client.execute(get)
-    val entity = response.getEntity
-    val content = entity.getContent
-    val data = scala.io.Source.fromInputStream(content).mkString
-    EntityUtils.consume(entity)
-    response.close
-    parse(data)
-  } 
-
-  def post(token: String, url: String, data: String) {
-    val httpPost = new HttpPost(url)
-    httpPost.addHeader(header, token)
-    val entity = new StringEntity(data, "UTF-8")
-    httpPost.setEntity(entity)
-    httpPost.setHeader("Content-type", "application/json; charset=UTF-8")
-    val response = client.execute(httpPost)
-    val code = response.getStatusLine().getStatusCode()
-    EntityUtils.consume(entity)
-    response.close
-  }
-  
-}
-
-class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
-  val source = opt[String](required = true)
-  val log = opt[String](required = true)
-  val drop = opt[Int](required = true)
-  val take = opt[Int](required = true)
-  verify()
 }
---
+++ src/main/scala/TopContainer.scala
@@ -0,0 +1,152 @@
+package edu.nyu.libraries.dlts.aspace
+
+
+object Http {
+
+  import com.typesafe.config._
+  import org.apache.http.HttpEntity
+  import org.apache.http.HttpResponse
+  import org.apache.http.client.{ ClientProtocolException, ResponseHandler }
+  import org.apache.http.client.config.RequestConfig
+  import org.apache.http.client.methods.{ HttpPost, HttpGet }
+  import org.apache.http.impl.client.{ HttpClientBuilder, CloseableHttpClient } 
+  import org.apache.http.impl.client.HttpClients
+  import org.apache.http.util.EntityUtils
+  import org.apache.http.entity.StringEntity
+  import org.json4s._
+  import org.json4s.jackson.JsonMethods._
+  import org.json4s.DefaultFormats
+
+
+  //initialize configuration
+  val conf = ConfigFactory.load()
+  val username = conf.getString("tcUpdater.username")
+  val password = conf.getString("tcUpdater.password")
+  val aspace = conf.getString("tcUpdater.aspaceApi")
+  
+  trait HttpSupport {
+
+    implicit val formats = DefaultFormats  
+    
+    val header = "X-ArchivesSpace-Session"
+    
+    val client = getClient
+
+    private def getClient(): CloseableHttpClient = {
+      val timeout = 2;
+      val config = RequestConfig.custom()
+        .setConnectTimeout(timeout * 1000)
+        .setConnectionRequestTimeout(timeout * 1000)
+        .setSocketTimeout(timeout * 1000).build();
+      
+      HttpClientBuilder.create().setDefaultRequestConfig(config).build()
+  
+    } 
+
+    val token = getToken(username, password)
+
+    private def getToken(username: String, password: String): String = {
+      try {
+        val authenticate = new HttpPost(aspace + "/users/" + username + "/login?password=" + password)
+        val response = client.execute(authenticate)
+        val entity = response.getEntity
+        val content = entity.getContent
+        val data = scala.io.Source.fromInputStream(content).mkString
+        val jarray = parse(data)
+        val askey = (jarray \ "session").extract[String]
+        EntityUtils.consume(entity)
+        response.close
+        askey
+      } catch {
+          case e: Exception => {
+            throw new Exception("Cannot Authenticate")
+            System.exit(1)
+            ""
+          }
+      }
+    }
+
+    def get(url: String): Option[JValue] = {
+      try{
+        val httpGet = new HttpGet(aspace + url)
+        httpGet.addHeader(header, token)
+        val response = client.execute(httpGet)
+        val entity = response.getEntity
+        val content = entity.getContent
+        val data = scala.io.Source.fromInputStream(content).mkString
+        EntityUtils.consume(entity)
+        response.close
+        Some(parse(data))
+      } catch {
+        case e: Exception => None
+      }
+    } 
+
+    def postJson(url: String, data: String): Option[Int] = {
+      try {
+        val httpPost = new HttpPost(aspace + url)
+        httpPost.addHeader(header, token)
+        val entity = new StringEntity(data, "UTF-8")
+        httpPost.setEntity(entity)
+        httpPost.setHeader("Content-type", "application/json; charset=UTF-8")
+        val response = client.execute(httpPost)
+        val code = response.getStatusLine().getStatusCode().toInt
+        EntityUtils.consume(entity)
+        response.close
+        Some(code)
+      } catch {
+        case e: Exception => None
+      }
+    }
+  }
+}
+
+object  CLI {
+  
+  import org.rogach.scallop._
+  import org.rogach.scallop.exceptions._
+
+  trait CLISupport { 
+    
+    def help(optionName: String) {
+      println(s"Error: Missing required option $optionName")
+      help()
+    }
+
+    def error(message: String) {
+      println(message)
+      println(help)
+    }
+
+    def help() {
+      println("usage: java -jar tcUpdate.jar [options]")
+      println("  options:")
+      println("    -s, --source, required\tpath to csv file to be input")
+      println("    -l, --log, required\t\tpath to log file to be written")
+      println("    -d, --drop, optional\tnumber of rows to skip from the beginning of csv file")
+      println("    -t, --take, optional\tnumber of rows to process from csv file")
+      println("    -h, --help\tprint this message")
+      System.exit(0)
+    }
+
+    class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+      val source = opt[String](required = true)
+      val log = opt[String](required = true)
+      val drop = opt[Int](required = true)
+      val take = opt[Int](required = true)
+      verify()
+    }
+
+    def getCLI(args: Array[String]): CLIConf = {
+      val cli = new CLIConf(args) {
+        override def onError(e: Throwable): Unit = e match {
+          case Help("") => help
+          case ScallopException(message) => error(message)
+          case RequiredOptionNotFound(optionName) => help(optionName)
+        }
+      }
+      
+      cli
+    }
+  }
+}
