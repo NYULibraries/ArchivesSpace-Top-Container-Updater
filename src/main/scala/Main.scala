9fc69447cded2322e39f9e4258b496318af7c989
Donald Mennerich <don.mennerich@nyu.edu>
2018-08-30 09:30:04 -0400

    code cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,10 @@
 package edu.nyu.libraries.dlts.aspace
 
-import CLI.CLISupport
-import Http.HttpSupport
-
 import java.io._
-import java.net.URI
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import org.json4s.DefaultFormats
-import org.rogach.scallop.exceptions._
 import scala.io.Source
+import Traits._
 
 object Main extends App with CLISupport with HttpSupport{
 
@@ -22,24 +17,19 @@ object Main extends App with CLISupport with HttpSupport{
   val drop = cli.drop()
   val take = cli.take()
  
-
   //initialize logs
   val logger = new FileWriter(new File(log))
   val errorLogger = new FileWriter(new File("errors.txt"))
-
-  //get authenticate
-  
 
   //process the csv file
   process
 
   def process() { 
     
-
     //initialize counter
     var i = drop
     
-    //itterate through csv file
+    //iterate through csv file
     Source.fromFile(csv).getLines.drop(drop).take(take).foreach{ line =>
       i = i + 1
       val cols = line.split(",")
@@ -47,18 +37,23 @@ object Main extends App with CLISupport with HttpSupport{
       val oldTC = JString(cols(1))
       val newTC = JString(cols(2))
 
-      try{
+      try {
 
         val json = get(ao).get
         val tc = json.\("instances")(0).\("sub_container").\("top_container").\("ref")
         val title = json.\("title").extract[String]
-        println(i + s": $ao: $title")    
-        
+        val info = ("$i\t$ao\t$title")    
+        print(info)
+
+        //check that the current top container URI is eqaul to the value from the spreadsheet
         (tc == oldTC) match {
           case true => {
+
+            //check that current top container URI is not already equal to the new value from the spreadsheet
             (tc != newTC) match {
               case true => {
               
+                //transform the json, replace the top container ref with new value
                 val updated = json.mapField {
                     case ("instances", JArray(head :: tail)) => ("instances", JArray(head.mapField {
                       case ("ref", oldTC) => ("ref", newTC)
@@ -67,32 +62,44 @@ object Main extends App with CLISupport with HttpSupport{
                     case otherwise => otherwise
                 }
 
+                //check that the updated json has the new target uri
                 (updated.\("instances")(0).\("sub_container").\("top_container").\("ref") == newTC) match {
                   case true => {
                     val updatedJson = (compact(render(updated)))
-                    postJson(ao, updatedJson)           
-                    logger.write(s"$ao $title updated ${oldTC.extract[String]} set to ${newTC.extract[String]} \n")
+                    postJson(ao, updatedJson)
+                    println("\tupdate success")           
+                    logger.write(s"$info\tsuccess\t\t${oldTC.extract[String]}\t${newTC.extract[String]} \n")
                     logger.flush
                   }
+                  //if not log result
                   case false => {
-                    logger.write(s"could not update $ao, target uri does not match spreadsheet \n")
+                    val msg = "\tskipped\tjson update failure"
+                    println(msg)
+                    logger.write(s"$info$msg\n")
                     logger.flush  
                   }
-               }
-            } 
-            
-            case false => {
-              logger.write(s"$ao $title not updated, target uri, ${newTC.extract[String]}, is the same as current uri \n")
-              logger.flush
+                }
+              }
+
+              //if not log the result
+              case false => {
+                val msg = "\tskipped\talready updated"
+                println(msg)
+                logger.write(s"$info$msg\n")
+                logger.flush
+              }
             }
           }
-        }
-          
-        case false => {
-          logger.write(s"$ao $title not updated, original uri, ${oldTC.extract[String]}, is not equal to current uri, ${tc.extract[String]} \n" )
-          logger.flush
+
+          //if not log the result  
+          case false => {
+            val msg = "\tskipped\turl mismatch"
+            println(msg)
+            logger.write(s"$info$msg\n")
+            logger.flush
           }
-        }
+        }  
+      //log any unexpected errors  
       } catch {
         case e: Exception => {
           errorLogger.write(e.toString + "\n")
