3e3df4f289241f0551747b6f23870228b7721357
Donald <don.mennerich@nyu.edu>
2018-10-03 10:27:31 -0400

    adding ignor file

---
+++ src/main/scala/CLI.scala
@@ -0,0 +1,101 @@
+package edu.nyu.libraries.dlts.aspace
+
+import org.rogach.scallop._
+import org.rogach.scallop.exceptions._
+import java.io.File
+import java.net.URL
+
+import com.typesafe.config.ConfigFactory
+
+import scala.io.StdIn
+
+object CLI {
+
+	trait CLISupport { 
+
+    val conf = ConfigFactory.load()
+
+    def help(optionName: String) {
+      println(s"Error: Missing required option $optionName")
+      help()
+    }
+
+	    def error(message: String) {
+      println(message)
+      println(help)
+    }
+
+    def help() {
+      println("usage: java -jar TCUpdate.jar [options]")
+      println("  options:")
+      println("    -s, --source, required\tpath to csv file to be input")
+      println("    -d, --drop, optional\tnumber of rows to skip from the beginning of csv file")
+      println("    -t, --take, optional\tnumber of rows to process from csv file")
+      println("    -h, --help\tprint this message")
+      System.exit(0)
+    }
+
+    class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+      val source = opt[String](required = true)
+      val drop = opt[Int](required = false)
+      val take = opt[Int](required = false)
+      verify()
+    }
+
+    def getAspaceOptions(args: Array[String]): AspaceSession = {
+
+      val cli = new CLIConf(args) {
+        override def onError(e: Throwable): Unit = e match {
+          case Help("") => help
+          case ScallopException(message) => error(message)
+          case RequiredOptionNotFound(optionName) => help(optionName)
+        }
+      }
+
+      val source = new File(cli.source.toOption.get)
+
+      dialogue(new AspaceSession(None, None, None, source, 2, cli.drop.toOption, cli.take.toOption, None, None))
+    }
+
+
+    private def dialogue(aspaceSession: AspaceSession): AspaceSession = {
+      val env = Some(new URL(conf.getString(s"client.$getEnv")))
+      val usr = getFromConsole("username")
+      val pswd = getFromConsole("password")
+
+      aspaceSession.copy(username = Some(usr), password= Some(pswd), url = env)
+    }
+
+    private def getEnv(): String = {
+      print("Enter the environment (prod, stage, dev), or q to quit: ")
+      val env = StdIn.readLine.toLowerCase.trim
+
+      (env == "q") match {
+        case true => System.exit(0) //quit the program
+        case false =>
+      }
+
+      env match {
+        case "prod" => env
+        case "stage" => env
+        case "dev" => env
+        case _ => {
+          println("Input is not a valid environment")
+          getEnv()
+        }
+      }
+
+    }
+
+    private def getFromConsole(str: String): String = {
+      print(s"Enter the $str, or q to quit: ")
+      val in = StdIn.readLine.trim
+      (in == "q") match {
+        case true => System.exit(0) //quit the program
+        case false =>
+      }
+
+      in
+    }
+	}
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,119 +1,130 @@
 package edu.nyu.libraries.dlts.aspace
 
-import java.io._
+import java.io.File
+import java.net.URL
+
+import CLI.CLISupport
+import AspaceClient.AspaceSupport
+import org.apache.http.impl.client.CloseableHttpClient
+import org.json4s.JsonAST.{JArray, JString}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import scala.io.Source
-import Traits._
+import org.json4s.DefaultFormats
+import scala.io.{BufferedSource, Source}
 
-object Main extends App with CLISupport with HttpSupport{
 
-  println("ACM Top Container Update Tool, v.0.1b")
+case class AspaceSession(username: Option[String], password: Option[String], url: Option[URL], source: File, timeout: Int, drop: Option[Int], take: Option[Int], client: Option[CloseableHttpClient], token: Option[String])
 
-  //initialize command line arguments
-  val cli = getCLI(args)  
-  val csv = cli.source()
-  val log = cli.log()
-  val drop = cli.drop()
-  val take = cli.take()
- 
-  //initialize logs
-  val logger = new FileWriter(new File(log))
-  val errorLogger = new FileWriter(new File("errors.txt"))
+object Main extends App with CLISupport with AspaceSupport {
 
-  //process the csv file
-  process
+  println("ACM Top Container Update Tool, v.0.3b")
 
-  def process() { 
-    
-    //initialize counter
-    var i = drop
-    
-    //iterate through csv file
-    Source.fromFile(csv).getLines.drop(drop).take(take).foreach{ line =>
-      i = i + 1
+  val session = setup(args)
+  val iterator = getIterator(session)
+  println("* successfully authenticated\n")
+  process(iterator, session)
+
+
+  private def setup(args: Array[String]): AspaceSession = {
+
+    //get a AspaceSession
+    val cli: AspaceSession = getAspaceOptions(args)
+
+    //check if the file exists
+    cli.source.exists() match {
+      case true =>
+      case false => {
+        println("* Source file does not exist, exiting.")
+        System.exit(1)
+      }
+    }
+
+    val session: AspaceSession = getSession(cli)
+
+    //check that there is a valid token
+    session.token match {
+      case Some(t) =>
+      case None => {
+        println("* Authentication failure, exiting.")
+        System.exit(1)
+      }
+    }
+
+    session
+  }
+
+  private def getIterator(session: AspaceSession): Iterator[String] = {
+    var i = Source.fromFile(session.source).getLines //figure out how to make this variable immutable
+
+    session.drop match {
+      case Some(n) => i = i.drop(n)
+      case None =>
+    }
+
+    session.take match {
+      case Some(n) => i = i.take(n)
+      case None =>
+    }
+
+    i
+  }
+
+  private def process(iterable: Iterator[String], session: AspaceSession): Unit = {
+    println("* processing")
+    iterable.foreach { line =>
       val cols = line.split(",")
-      val ao = cols(0)
+      val aoUrl = cols(0)
       val oldTC = JString(cols(1))
       val newTC = JString(cols(2))
 
       try {
 
-        val json = get(ao).get
-        val tc = json.\("instances")(0).\("sub_container").\("top_container").\("ref")
-        val title = json.\("title").extract[String]
-        val info = (s"$i. $ao\t$title\t")    
-        print(info)
-
-        //check that the current top container URI is eqaul to the value from the spreadsheet
-        (tc == oldTC) match {
-          case true => {
+        get(session, aoUrl) match {
+          //retrieve ao from aspace
+          case Some(json) => {
+            val tc = json.\("instances")(0).\("sub_container").\("top_container").\("ref")
+            val title = json.\("title").extract[String]
+            val info = (s"$aoUrl\t$title")
 
             //check that current top container URI is not already equal to the new value from the spreadsheet
             (tc != newTC) match {
               case true => {
-              
-                //transform the json, replace the top container ref with new value
-                val updated = json.mapField {
-                    case ("instances", JArray(head :: tail)) => ("instances", JArray(head.mapField {
-                      case ("ref", oldTC) => ("ref", newTC)
-                      case otherwise => otherwise
-                    } :: tail))
-                    case otherwise => otherwise
-                }
-
-                //check that the updated json has the new target uri
-                (updated.\("instances")(0).\("sub_container").\("top_container").\("ref") == newTC) match {
+                //check that the current top container URI is eqaul to the value from the spreadsheet
+                (tc == oldTC) match {
                   case true => {
-                    val updatedJson = (compact(render(updated)))
-                    postJson(ao, updatedJson)
-                    println("updated")           
-                    logger.write(s"$info success\t\t${oldTC.extract[String]}\t${newTC.extract[String]} \n")
-                    logger.flush
+                    //transform the json, replace the top container ref with new value
+                    val updated = json.mapField {
+                      case ("instances", JArray(head :: tail)) => ("instances", JArray(head.mapField {
+                        case ("ref", oldTC) => ("ref", newTC)
+                        case otherwise => otherwise
+                      } :: tail))
+                      case otherwise => otherwise
+                    }
+                    //check that the updated json has the new target uri
+                    (updated.\("instances")(0).\("sub_container").\("top_container").\("ref") == newTC) match {
+                      //post the updated object
+                      case true => postJson(session, aoUrl, (compact(render(updated))))
+                      case false => println(s"$title json object update failed")
+                    }
                   }
-                  //if not log result
+
                   case false => {
-                    val msg = "skipped\tjson update failure"
-                    println(msg)
-                    logger.write(s"$info$msg\n")
-                    logger.flush  
+                    println("url mismatch")
                   }
                 }
               }
-
-              //if not log the result
-              case false => {
-                val msg = "skipped\talready updated"
-                println(msg)
-                logger.write(s"$info$msg\n")
-                logger.flush
-              }
+              case false => { println(s"$title already updated") }
             }
           }
+          case None => "AO does not exist on server"
+        }
 
-          //if not log the result  
-          case false => {
-            val msg = "skipped\turl mismatch"
-            println(msg)
-            logger.write(s"$info$msg\n")
-            logger.flush
-          }
-        }  
-      //log any unexpected errors  
       } catch {
         case e: Exception => {
-          errorLogger.write(e.toString + "\n")
-          errorLogger.flush
+          println("** " + e.toString + "\n")
         }
       }
     }
-
-    //cleanup
-    logger.flush
-    errorLogger.flush
-    logger.close
-    errorLogger.close
-    client.close
   }
 
 }
